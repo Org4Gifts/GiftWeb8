80c3fa76f259704f48e3f1e5df4460fcc2653ee7
Administrator <Administrator@Asus>
2016-12-23 19:30:33 +0800

    12/23 Modify LoginServlet.java

--- src/com/giftweb/controller/LoginServlet.java
+++ src/com/giftweb/controller/LoginServlet.java
@@ -1,6 +1,8 @@
 package com.giftweb.controller;
 
 import java.io.IOException;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import tw.youth.project.gift2016.consts.ConstValue;
 import tw.youth.project.gift2016.func.Login;
 import tw.youth.project.gift2016.sql.DBManager;
 import tw.youth.project.gift2016.sql.SQLCmd;
+import tw.youth.project.gift2016.sql.user.AUSER;
 
 /**
  * Servlet implementation class LoginServlet
@@ -19,6 +22,7 @@ import tw.youth.project.gift2016.sql.SQLCmd;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static DBManager manager;
+	private static Map<String, Object[]> changePassList;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -52,10 +56,10 @@ public class LoginServlet extends HttpServlet {
 
 		String username = request.getParameter("username") == null ? "" : request.getParameter("username");
 		String pass = request.getParameter("pass") == null ? "" : request.getParameter("pass");
-		
+
 		System.out.println("username = " + username);
 		System.out.println("pass = " + pass);
-		
+
 		Login login = new Login(manager, username, pass);
 
 		String action = request.getParameter("action"); // 取得
@@ -69,24 +73,43 @@ public class LoginServlet extends HttpServlet {
 		// 前端網頁 1.登入
 		if ("Login".equals(action)) {
 			String check = login.checkLogin();
-			if("登入失敗".equals(check)){
+			if ("登入失敗".equals(check)) {
 				request.setAttribute("error", check);
-				request.getRequestDispatcher("/login.jsp").forward(request, response);				
-			}else{
+				request.getRequestDispatcher("/login.jsp").forward(request, response);
+			} else {
 				request.getRequestDispatcher("/index.jsp").forward(request, response);
 			}
 		}
-		
+
 		// 前端網頁 2.忘記密碼
 		if ("forgot".equals(action)) {
 
-			String info = login.forgotPass(manager, request.getParameter("email"));
+			// String info = login.forgotPass(manager,
+			// request.getParameter("email"),);
+			//
+			// System.out.println("info = " + info);
+			// System.out.println("mail = " + request.getParameter("email"));
 
-			System.out.println("info = " + info);
-			System.out.println("mail = " + request.getParameter("email"));
+			// request.setAttribute("error", info);
+			// request.getRequestDispatcher("/login.jsp").forward(request,
+			// response);
 
-			request.setAttribute("error", info);
+			Object[] info = login.forgotPass(manager, request.getParameter("email"), "");
+			request.setAttribute("error", info[0]);
 			request.getRequestDispatcher("/login.jsp").forward(request, response);
 		}
+
+		// 前端網頁 3.修改密碼
+		if ("changePwd".equals(action)) {
+			String oldPasswd = request.getParameter("oldPasswd");
+			String newPasswd1 = request.getParameter("newPasswd1");
+			String newPasswd2 = request.getParameter("newPasswd2");			
+
+			String info = login.changPassword(manager, oldPasswd, newPasswd1, newPasswd2);
+
+			request.setAttribute("error", info);
+			request.getRequestDispatcher("/changePwd.jsp").forward(request, response);
+		}
+
 	}
 }
